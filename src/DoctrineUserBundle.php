0fdfe4b5d55cc8f442ea744cc18250e4e441283a
github-ci <you@example.com>
2025-06-11 11:25:13 +0800

    update

--- src/DoctrineUserBundle.php
+++ src/DoctrineUserBundle.php
@@ -2,6 +2,7 @@
 
 namespace Tourze\DoctrineUserBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
@@ -12,6 +13,7 @@ class DoctrineUserBundle extends Bundle implements BundleDependencyInterface
     public static function getBundleDependencies(): array
     {
         return [
+            DoctrineBundle::class => ['all' => true],
             DoctrineEntityCheckerBundle::class => ['all' => true],
             SecurityBundle::class => ['all' => true],
         ];
